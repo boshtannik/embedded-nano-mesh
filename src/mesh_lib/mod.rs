5b16b5b9b8b253b78fd76f204899b8e84e62fcc3
boshtannik <boshtannik@gmail.com>
2024-03-03 17:14:23 +0200

    Move out examples into separate repo. Add packet state to exported as public

--- src/mesh_lib/mod.rs
+++ src/mesh_lib/mod.rs
@@ -1,7 +1,7 @@
 mod node;
 
 pub use node::{
-    AddressType, LifeTimeType, Node, NodeConfig, NodeString, NodeUpdateError, SendError,
-    SpecialSendError, MULTICAST_RESERVED_IDENTIFIER,
+    AddressType, LifeTimeType, Node, NodeConfig, NodeString, NodeUpdateError, PacketState,
+    SendError, SpecialSendError, MULTICAST_RESERVED_IDENTIFIER,
 };
 pub use platform_millis::ms;
